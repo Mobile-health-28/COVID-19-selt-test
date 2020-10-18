e855327f8f455bcdb11c5e803ee792df46d60610
firminApp <firminapp@gmail.com>
2020-10-18 18:29:01 +0100

    add questions features and fix cors issues

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,6 +5,13 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
+                \App\Http\Middleware\EncryptCookies::class,
+                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                \Illuminate\Session\Middleware\StartSession::class,
+                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                \App\Http\Middleware\VerifyCsrfToken::class,
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
                 'title' => 'COVID-19 Self test  API',
             ],
 
@@ -118,12 +125,12 @@ return [
                 ],
                 */
 
-                /* Open API 3.0 support
+                //Open API 3.0 support
                 'passport' => [ // Unique name of security
                     'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
                     'description' => 'Laravel passport oauth2 security.',
                     'in' => 'header',
-                    'scheme' => 'https',
+                    'scheme' => 'http',
                     'flows' => [
                         "password" => [
                             "authorizationUrl" => config('app.url') . '/oauth/authorize',
@@ -133,7 +140,7 @@ return [
                         ],
                     ],
                 ],
-                */
+                
             ],
             'security' => [
                 /*
