69b30dad3c6178ed6bd05c39a2bebbb42d6843ec
firminApp <firminapp@gmail.com>
2020-10-19 16:38:55 +0100

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
-        \App\Http\Middleware\Cors::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\ForceJsonResponse::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
