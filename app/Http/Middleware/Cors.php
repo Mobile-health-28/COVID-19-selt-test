56a9af15d5573185277f50f56c392bedda7f0585
firminApp <firminapp@gmail.com>
2020-10-19 16:22:40 +0100

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,6 @@ class Cors
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', '*');
+        ->header('Access-Control-Allow-Headers', 'X-Requested-With, XMLHttpRequest, Content-Type, X-Token-Auth, Authorization');
     }
 }
