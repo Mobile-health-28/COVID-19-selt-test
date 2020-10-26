4f6f2580ce2579ac03d44ef33b3731ac6944128a
Kibirige Junior Lawrence <aanga2@gmail.com>
2020-10-26 18:16:36 +0300

    Created and Fulfilled Location History Requirements where location history must happen after spending an hour in area

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Location extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'location_history';
+    protected $fillable = [
+        'latitude',
+        'longitude',
+        'location_name',
+        'user_id'
+    ];
 }
