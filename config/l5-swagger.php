18ad766dc4975940218dab50842e550ab907be91
Kibirige Junior Lawrence <aanga2@gmail.com>
2020-10-26 18:14:04 +0300

    Updated API_DOCS with location tracking data

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -51,7 +51,7 @@ return [
              * Middleware allows to prevent unexpected access to API documentation
             */
             'middleware' => [
-                'api' => [
+                'auth:api' => [
                     \App\Http\Middleware\EncryptCookies::class,
                     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
                     \Illuminate\Session\Middleware\StartSession::class,
